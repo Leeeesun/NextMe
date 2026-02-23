2d5db6df20dca98fa95fdf26598c12117920a678
Mask59 <how.mask@gmail.com>
2026-02-23 15:29:37 +0800

    feat: Create initial personal portfolio page with bio, social links, and an image gallery.

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
     <section>
       <section className="sm:px-28 sm:pt-8">
         <Image
-          alt={'Hamster1963'}
+          alt={'Lesun'}
           src={avatar}
           height={64}
           width={64}
@@ -23,13 +23,13 @@ export default async function Page() {
           priority
         />
         <h1 className="mb-1 text-xl font-medium tracking-tighter">
-          Hamster1963
+          Lesun
         </h1>
         <p className="prose prose-neutral text-sm dark:prose-invert">
           Developer, guitarist, and creator.
         </p>
         <p className={'mb-6 pt-10 text-lg font-medium tracking-tight'}>
-          👋 Hi, I'm Hamster1963.
+          👋 Hi, I'm Lesun.
           <br />
           Love to write code and write articles.
           <br />
@@ -38,7 +38,7 @@ export default async function Page() {
         </p>
         <NowPlayingInit />
         <div className={'mt-6 flex items-center'}>
-          <Link href="https://github.com/hamster1963" target="_blank">
+          <Link href="https://github.com/Leeeesun" target="_blank">
             <button
               className={
                 'mr-4 flex items-center justify-center rounded-xl border-[0.5px] border-neutral-200 p-2 text-xs font-medium shadow-[0_2px_2px_rgba(0,0,0,0.04)] transition-all duration-300 hover:bg-stone-100 hover:shadow-none dark:border-neutral-700 dark:bg-neutral-700/20 dark:hover:bg-neutral-900/20'
@@ -50,7 +50,7 @@ export default async function Page() {
           </Link>
           <span className="mr-2 h-2 w-2 rounded-full bg-blue-500"></span>
           <Link
-            href={'https://home.buycoffee.top'}
+            href={'https://status.youranzi.de'}
             target="_blank"
             className="text-xs font-medium"
           >
@@ -58,7 +58,7 @@ export default async function Page() {
           </Link>
           <p className="mx-1 text-xs font-medium">/</p>
           <Link
-            href={'https://changelog.buycoffee.top'}
+            href={'#'}
             target="_blank"
             className="text-xs font-medium"
           >
